a16ef314c3133377efe9008167a258c0bdc48529
Sam Shores <plaidphantom@gmail.com>
2019-03-20 00:56:35 -0500

    php

---
+++ code-samples/php.php
@@ -0,0 +1,24 @@
+<?php
+/* vim:set softtabstop=4 shiftwidth=4 expandtab: */
+
+require_once 'lib/init.php';
+
+switch ($_REQUEST['action']) {
+    case 'delete':
+        if (false) {
+            break;
+        }
+    break;
+}
+
+$variable = scrub_in($_REQUEST['variable']);
+dothing('asdf', 1, null);
+echo 'asdf';
+
+?>
+
+<html>
+    <body>
+        <? echo $asdf; ?>
+    </body>
+</html>

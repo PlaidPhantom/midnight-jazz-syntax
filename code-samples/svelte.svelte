95d12cf630fea373b752972c0d41d39da3cf7302
Sam Shores <plaidphantom@gmail.com>
2020-02-20 00:24:04 -0600

    fix some nested styles, add svelte example (fixes #17 & #19)

---
+++ code-samples/svelte.svelte
@@ -0,0 +1,25 @@
+<p>
+    <button on:click={doSomething()}>Text</button>
+</p>
+
+<style>
+
+button.btn {
+    font-weight: bold;
+}
+
+</style>
+
+<script>
+
+import { onMount } from 'svelte';
+
+function doSomething() {
+    // TODO asdfasfd
+}
+
+onMount(() => {
+    doSomething();
+})
+
+</script>
